a4ef401560d26cd924d5dea53720bce1d0950daa
MartinShift <mori.steamer@gmail.com>
2025-02-11 15:33:56 +0100

    fix 1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['jquery', 'bootstrap'],
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
